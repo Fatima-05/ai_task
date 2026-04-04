e398cd88a753542f940bc0a897c79c147214ff4f
Fatima-05 <fatima.tu.zahra.005@gmail.com>
2026-04-05 00:10:45 +0500

    starting room reservation task

---
+++ Main2.java
@@ -0,0 +1,11 @@
+class RoomReservation {
+    private int id;
+    private String name;
+    private int services;
+    private int days;
+    private double bill;
+    private double tax;
+    private int ranking;
+
+    
+}
