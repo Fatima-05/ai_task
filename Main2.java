e85964994041f7493ad93ecab77dda67884fbac0
Fatima-05 <fatima.tu.zahra.005@gmail.com>
2026-04-05 00:14:00 +0500

    adding getters

--- Main2.java
+++ Main2.java
@@ -7,5 +7,22 @@ class RoomReservation {
     private double tax;
     private int ranking;
 
-    
+    void setId(int id){ 
+    this.id=id; 
+    }
+
+    void setName(String name){
+        this.name=name; 
+    }
+
+    void setServices(int services){ 
+        this.services=services; 
+    }
+
+    void setDays(int days){ 
+        this.days=days;
+    }
+    void setRanking(int ranking){ 
+        this.ranking=ranking; 
+    }
 }
